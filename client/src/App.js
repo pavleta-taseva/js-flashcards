63bf2df83e9e003eb6948cf4b154f0b9d58fe90a
Kumiho <pavleta.taseva@gmail.com>
2021-11-02 19:04:17 +0200

    add category animation effect

--- client/src/App.js
+++ client/src/App.js
@@ -4,6 +4,9 @@ import Category from './components/Category/Category.js';
 function App() {
   return (
     <div className="App">
+      <div>
+        <img src="../../" alt="" />
+      </div>
       <Category />
     </div>
   );
