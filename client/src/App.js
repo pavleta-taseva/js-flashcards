7100d711a8ae83292e5daab8b81132729a39a3fb
Kumiho <pavleta.taseva@gmail.com>
2021-11-03 09:50:02 +0200

    add nav bar with active link

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,11 @@
 import './App.css';
 import Category from './components/Category/Category.js';
+import Navbar from './components/Navbar/Navbar.js';
 
 function App() {
   return (
     <div className="App">
+      <Navbar />
       <Category />
     </div>
   );
---
+++ client/src/components/Navbar/Navbar.js
@@ -0,0 +1,59 @@
+import React from 'react';
+import '../Navbar/Navbar.css';
+import { Link } from 'react-router-dom';
+import { useLocation } from "react-router-dom";
+
+function Navbar() {
+    //assigning location variable
+    const location = useLocation();
+
+    //destructuring pathname from location
+    const { pathname } = location;
+
+    //Javascript split method to get the name of the path in array
+    const splitLocation = pathname.split("/");
+
+    return (
+        <div className="nav">
+            <ul>
+                {/* Checking the current path name using javascript ternary operator and if true adding active classname to it */}
+                <li className={splitLocation[1] === "" ? "active" : ""}>
+                    <Link to="/">
+                        <span className="nav-icon"><ion-icon name="home-outline"></ion-icon></span>
+                        <span className="nav-item-title">Home</span>
+                    </Link>
+                </li>
+
+                <li className={splitLocation[1] === "register" ? "active" : ""}>
+                    <Link to="/register">
+                        <span className="nav-icon"><ion-icon name="person-add-outline"></ion-icon></span>
+                        <span className="nav-item-title">Register</span>
+                    </Link>
+                </li>
+
+                <li className={splitLocation[1] === "login" ? "active" : ""}>
+                    <Link to="/login">
+                        <span className="nav-icon"><ion-icon name="log-in-outline"></ion-icon></span>
+                        <span className="nav-item-title">Login</span>
+                    </Link>
+                </li>
+
+                <li className={splitLocation[1] === "profile" ? "active" : ""}>
+                    <Link to="/profile">
+                        <span className="nav-icon"><ion-icon name="person-outline"></ion-icon></span>
+                        <span className="nav-item-title">Profile</span>
+                    </Link>
+                </li>
+
+                <li className={splitLocation[1] === "logout" ? "active" : ""}>
+                    <Link to="/logout">
+                        <span className="nav-icon"><ion-icon name="log-out-outline"></ion-icon></span>
+                        <span className="nav-item-title">Logout</span>
+                    </Link>
+                </li>
+            </ul>
+        </div>
+    )
+}
+
+export default Navbar;
