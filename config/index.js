e54fd85ad2f7f80c53423bf67a2ffe4b51d72c12
Kumiho <pavleta.taseva@gmail.com>
2021-11-06 10:23:59 +0200

    commit

--- config/index.js
+++ config/index.js
@@ -1,4 +1,6 @@
 module.exports = {
     PORT: process.env.PORT || 5000,
+    DB_CONNECTION_STRING: 'mongodb+srv://cardsAdmin:p35Sv2KtFpaXMvw@flashcards.gjgys.mongodb.net/js-flashcards',
+    TOKEN_SECRET: 'v6tQCHqxD9P5K4oLa',
     COOKIE_NAME: 'Session_Token'
 }
