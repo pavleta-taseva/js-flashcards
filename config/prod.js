34f762cde1ff7e287de1eec57743312200bb9454
Kumiho <pavleta.taseva@gmail.com>
2021-11-12 09:23:14 +0200

    authToken

--- config/prod.js
+++ config/prod.js
@@ -3,5 +3,5 @@ module.exports = {
     PORT: process.env.PORT || 5000,
     DB_CONNECTION_STRING: process.env.DB_CONNECTION_STRING,
     TOKEN_SECRET: process.env. TOKEN_SECRET,
-    COOKIE_NAME: 'Session_Token',
+    COOKIE_NAME: 'authToken',
 }
