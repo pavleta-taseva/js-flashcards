4a50ac9573401a65d4947ced26ba3c03b5628749
Kumiho <pavleta.taseva@gmail.com>
2021-11-13 21:00:51 +0200

    crutial update

--- client/src/index.js
+++ client/src/index.js
@@ -1,8 +1,8 @@
+import reportWebVitals from './reportWebVitals';
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
 import { BrowserRouter as Router } from "react-router-dom";
 
 ReactDOM.render(
