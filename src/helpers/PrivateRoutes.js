b323b4a0fddddd4ad9375c0a77c8037736aa440b
Kumiho <pavleta.taseva@gmail.com>
2021-12-14 12:06:48 +0200

    authContext fixed

--- src/helpers/PrivateRoutes.js
+++ src/helpers/PrivateRoutes.js
@@ -1,10 +1,9 @@
 import { Navigate, Outlet } from 'react-router-dom';
-import { useAuth } from '../contexts/AuthContext.js';
 import * as authService from '../services/authService.js';
 
 const PrivateRoute = () => {
    const isAuthenticated = authService.isAuthenticated();
-   
+
     return isAuthenticated ? <Outlet /> : <Navigate to="/login" />
 }
 
