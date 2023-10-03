5fbd05ad8adcd3808f98cd81eeff41629ada5b77
PavletaT <pavleta.taseva@evalue.cz>
2023-10-03 11:03:48 +0300

    Add contact form - test Cms app service

--- src/App.js
+++ src/App.js
@@ -1,35 +1,35 @@
-import React from 'react';
-import { Route, Routes } from 'react-router-dom';
-import Category from './components/User/Category/Category.js';
-import Navbar from './components/Navbar.js';
-import Home from './components/User/Home/Home.js';
-import LandingPage from './components/LandingPage.js';
-import Register from './components/User/Register/Register.js';
-import Login from './components/User/Login/Login.js';
-import Profile from './components/User/Profile/Profile.js';
-import Collections from './components/Collections.js';
-import FlashcardsBasic from './components/FlashcardsBasic.js';
-import FlashcardsAdvanced from './components/FlashcardsAdvanced.js';
-import FlashcardsWeb from './components/FlashcardsWeb.js';
-import Edit from './components/Owner/Edit/Edit.js';
-import Create from './components/Owner/Create/Create.js';
-import MyCards from './components/Owner/MyCards/MyCards.js';
-import Practice from './components/User/Practice/Practice.js';
-import Details from './components/User/Details/Details.js';
-import OwnerDetails from './components/Owner/OwnerDetails/OwnerDetails.js';
-import Footer from './components/Footer.js';
-import { ReactNotifications } from 'react-notifications-component'
-import NotFoundPage from './components/NotFoundPage.js';
-import { AuthProvider } from './contexts/AuthContext.js';
-import PrivateRoutes from './helpers/PrivateRoutes.js';
-import PublicRoutes from './helpers/PublicRoutes.js';
-import 'react-notifications-component/dist/theme.css';
+import React from "react";
+import { Route, Routes } from "react-router-dom";
+import Category from "./components/User/Category/Category.js";
+import Navbar from "./components/Navbar.js";
+import Home from "./components/User/Home/Home.js";
+import LandingPage from "./components/LandingPage.js";
+import Register from "./components/User/Register/Register.js";
+import Login from "./components/User/Login/Login.js";
+import Profile from "./components/User/Profile/Profile.js";
+import Collections from "./components/Collections.js";
+import FlashcardsBasic from "./components/FlashcardsBasic.js";
+import FlashcardsAdvanced from "./components/FlashcardsAdvanced.js";
+import FlashcardsWeb from "./components/FlashcardsWeb.js";
+import Edit from "./components/Owner/Edit/Edit.js";
+import Create from "./components/Owner/Create/Create.js";
+import MyCards from "./components/Owner/MyCards/MyCards.js";
+import Practice from "./components/User/Practice/Practice.js";
+import Details from "./components/User/Details/Details.js";
+import OwnerDetails from "./components/Owner/OwnerDetails/OwnerDetails.js";
+import Footer from "./components/Footer.js";
+import { ReactNotifications } from "react-notifications-component";
+import NotFoundPage from "./components/NotFoundPage.js";
+import { AuthProvider } from "./contexts/AuthContext.js";
+import PrivateRoutes from "./helpers/PrivateRoutes.js";
+import PublicRoutes from "./helpers/PublicRoutes.js";
+import ContactForm from "./components/ContactForm.js";
+import "react-notifications-component/dist/theme.css";
 
 const App = () => {
-
   return (
     <AuthProvider>
-      <div className="App">
+      <div className='App'>
         <ReactNotifications />
         <Navbar />
         <main>
@@ -40,21 +40,34 @@ const App = () => {
               <Route path='/login' element={<Login />} />
               <Route path='/collections' element={<Collections />} />
               <Route path='/collections/:number' element={<Collections />} />
+              <Route path='/contact' element={<ContactForm />} />
             </Route>
-            
+
             <Route path='/details/:id' element={<Details />} />
-            
+
             <Route element={<PrivateRoutes />}>
               <Route path='/home' element={<Home />}>
                 <Route path='/home/categories' element={<Category />} />
               </Route>
               <Route path='/profile/:userId' element={<Profile />} />
               <Route path='/flashcards-basic' element={<FlashcardsBasic />} />
-              <Route path='/flashcards-basic/:number' element={<FlashcardsBasic />} />
-              <Route path='/flashcards-advanced' element={<FlashcardsAdvanced />} />
-              <Route path='/flashcards-advanced/:number' element={<FlashcardsAdvanced />} />
+              <Route
+                path='/flashcards-basic/:number'
+                element={<FlashcardsBasic />}
+              />
+              <Route
+                path='/flashcards-advanced'
+                element={<FlashcardsAdvanced />}
+              />
+              <Route
+                path='/flashcards-advanced/:number'
+                element={<FlashcardsAdvanced />}
+              />
               <Route path='/flashcards-web' element={<FlashcardsWeb />} />
-              <Route path='/flashcards-web/:number' element={<FlashcardsWeb />} />
+              <Route
+                path='/flashcards-web/:number'
+                element={<FlashcardsWeb />}
+              />
               <Route path='/flashcards/create' element={<Create />} />
               <Route path='/details/:ownerId/:id' element={<OwnerDetails />} />
               <Route path='/my-cards/:userId' element={<MyCards />} />
@@ -71,6 +84,6 @@ const App = () => {
       </div>
     </AuthProvider>
   );
-}
+};
 
 export default App;
---
+++ src/components/ContactForm.js
@@ -0,0 +1,85 @@
+import React, { useState } from "react";
+
+const ContactForm = () => {
+  const [error, setError] = useState(false);
+
+  const onSubmit = async (event) => {
+    event.preventDefault();
+
+    const form = event.target;
+    const data = new FormData(form);
+    const value = Object.fromEntries(data.entries());
+
+    try {
+      const postFormData = await fetch(`${process.env.REACT_APP_MAIL_SENDER}`, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+          "Access-Control-Allow-Credentials": true,
+          Connection: "keep-alive",
+          credentials: "include",
+        },
+        body: JSON.stringify(value),
+      });
+
+      return postFormData;
+    } catch (err) {
+      setError(err.message);
+    }
+  };
+
+  return (
+    <section className='login-section'>
+      <div className='login-container'>
+        <div className='loginForm-container'>
+          <form onSubmit={onSubmit}>
+            {error && <div>{`Error: ${error}`}</div>}
+            <h1>Send email</h1>
+            <p>Please enter your email data</p>
+            <label>Sender</label>
+            <br></br>
+            <div className='icon'>
+              <input
+                name='sender'
+                type='email'
+                className='login-password'
+              ></input>
+              <br></br>
+            </div>
+
+            <label>Subject</label>
+            <br></br>
+            <div className='icon'>
+              <input
+                name='subject'
+                className='login-password'
+                type='text'
+              ></input>
+              <br></br>
+            </div>
+            <textarea
+              name='body'
+              className='login-password'
+              type='text'
+              placeholder='Message'
+              style={{
+                height: "200px",
+                width: "90%",
+                marginBottom: "20px",
+                padding: "10px",
+                color: "black",
+              }}
+            ></textarea>
+            <div>
+              <button type='submit' className='createBtn'>
+                Submit email
+              </button>
+            </div>
+          </form>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default ContactForm;
