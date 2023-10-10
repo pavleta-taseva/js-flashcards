a80f16391d4bcf6d6ba5af3b954e8f08f9020597
PavletaT <pavleta.taseva@evalue.cz>
2023-10-10 13:57:55 +0300

    simple form test

--- src/components/ContactForm.js
+++ src/components/ContactForm.js
@@ -1,51 +1,51 @@
-import { useState } from "react";
-import { useNavigate } from "react-router-dom";
+// import { useState } from "react";
+// import { useNavigate } from "react-router-dom";
 
 const ContactForm = () => {
-  const [error, setError] = useState(false);
-  const navigate = useNavigate();
+  // const [error, setError] = useState(false);
+  // const navigate = useNavigate();
 
-  const onSubmit = async (event) => {
-    event.preventDefault();
+  // const onSubmit = async (event) => {
+  //   event.preventDefault();
 
-    const form = event.target;
-    const data = new FormData(form);
-    const value = Object.fromEntries(data.entries());
-    const projectId = process.env.REACT_APP_PROJECT_ID;
-    const emailData = {
-      ...value,
-      projectId,
-    };
+  //   const form = event.target;
+  //   const data = new FormData(form);
+  //   const value = Object.fromEntries(data.entries());
+  //   const projectId = process.env.REACT_APP_PROJECT_ID;
+  //   const emailData = {
+  //     ...value,
+  //     projectId,
+  //   };
 
-    try {
-      const postFormData = await fetch(`${process.env.REACT_APP_MAIL_SENDER}`, {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-          "Access-Control-Allow-Credentials": true,
-          Connection: "keep-alive",
-          credentials: "include",
-        },
-        body: JSON.stringify(emailData),
-      });
+  //   try {
+  //     const postFormData = await fetch(`${process.env.REACT_APP_MAIL_SENDER}`, {
+  //       method: "POST",
+  //       headers: {
+  //         "Content-Type": "application/json",
+  //         "Access-Control-Allow-Credentials": true,
+  //         Connection: "keep-alive",
+  //         credentials: "include",
+  //       },
+  //       body: JSON.stringify(emailData),
+  //     });
 
-      if (
-        (postFormData && postFormData.ok) ||
-        postFormData.statusText === "Created"
-      ) {
-        setError(false);
-        form.reset();
-        navigate("/");
-      }
+  //     if (
+  //       (postFormData && postFormData.ok) ||
+  //       postFormData.statusText === "Created"
+  //     ) {
+  //       setError(false);
+  //       form.reset();
+  //       navigate("/");
+  //     }
 
-      if (!postFormData.ok) {
-        setError(`${postFormData.statusText}` + error);
-      }
-      return postFormData;
-    } catch (err) {
-      setError(err.message);
-    }
-  };
+  //     if (!postFormData.ok) {
+  //       setError(`${postFormData.statusText}` + error);
+  //     }
+  //     return postFormData;
+  //   } catch (err) {
+  //     setError(err.message);
+  //   }
+  // };
 
   return (
     <section className='login-section'>
